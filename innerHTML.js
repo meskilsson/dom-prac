9a3ec9d958c47fba68b9d21e935c5eba38b9106f
Mattias Eskilsson <mattiaseskilsson@hotmail.se>
2025-10-15 15:57:06 +0200

    textContent

--- innerHTML.js
+++ innerHTML.js
@@ -1,18 +1,18 @@
 
 
-const h1 = document.querySelector('.h1');
+// const h1 = document.querySelector('.h1');
 
-h1.innerHTML = "<h1> new h1 header </h1>";
+// h1.innerHTML = "<h1> new h1 header </h1>";
 
-const h2 = document.querySelector(".h2");
+// const h2 = document.querySelector(".h2");
 
-h2.innerHTML = "<h2>H2H2H2H2</h2>";
+// h2.innerHTML = "<h2>H2H2H2H2</h2>";
 
-let h3 = document.querySelector('.h3');
+// let h3 = document.querySelector('.h3');
 
-h3.innerHTML = "<h3> what could this be? </h3>";
+// h3.innerHTML = "<h3> what could this be? </h3>";
 
 
-let ptag = document.getElementById('paragraf');
+// let ptag = document.getElementById('paragraf');
 
-ptag.innerHTML = "<p>I am changed </p>";
+// ptag.innerHTML = "<p>I am changed </p>";
---
+++ textContent.js
@@ -0,0 +1,32 @@
+
+let textContentContainer = document.querySelector('.textContent');
+
+// textContentContainer.setAttribute('style', 'display: flex; justify-content: center; align-items: center;')
+
+let h1 = document.getElementById('texth1');
+
+h1.textContent = "This is maybe an h1";
+
+let h2 = document.getElementById('texth2');
+
+h2.textContent = "Is this working?";
+
+let h3 = document.querySelector('.texth3');
+
+
+h3.textContent = "am I green?";
+h3.style.color = "green";
+
+
+let h4 = document.querySelector('.texth4');
+
+h4.textContent = "I want to move to the center.";
+
+h4.style.color = 'red';
+
+
+let pirrigriff = document.querySelector('.textp');
+
+pirrigriff.textContent = "aj em a pirrigrif tixt, pls mek me blue";
+
+pirrigriff.style.color = "blue";
